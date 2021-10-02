a2e62d27327c99316c67212c6f55f08643e1daad
SergeyBirukov <birukovsergej121@gmail.com>
2021-10-02 23:19:49 +0500

    Add doors

---
+++ Assets/Door.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public GameObject Out;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void Open()
+    {
+        State = States.open;
+    }
+
+    public void Close()
+    {
+        State = States.close;
+    }
+
+    public void Idle()
+    {
+        State = States.idle;
+    }
+
+    public States State
+    {
+        get
+        {
+            return (States)animator.GetInteger("state");
+        }
+        set
+        {
+            animator.SetInteger("state", (int)value);
+        }
+    }
+}
+
+public enum States
+{
+    idle,
+    open, 
+    close
+}
