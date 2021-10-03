a413c5a390c3a5ee90c975292b0c94baa28da8a2
Axonny <KawaiiAxonny@gmail.com>
2021-10-03 23:59:32 +0500

    fix NullReference after load scene

--- Assets/Entity/Player.cs
+++ Assets/Entity/Player.cs
@@ -37,7 +37,7 @@ public class Player : Entity
      private void Awake()
      {
           Keys = new List<string>();
-          weapon = gameObject.AddComponent<Weapon>();
+          weapon = gameObject.GetComponent<Weapon>();
           weapon.weaponPos = attackPoint.transform;
           rigidbody = GetComponent<Rigidbody2D>();
           spriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
