3b70dc0b6d06dff0b285b22889f90e54e4e1aa50
Axonny <KawaiiAxonny@gmail.com>
2021-10-03 22:43:00 +0500

    fix sidedoors

--- Assets/SideDoor/SideDoor.cs
+++ Assets/SideDoor/SideDoor.cs
@@ -1,36 +1,29 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SideDoor : MonoBehaviour
 {
     [SerializeField] private BoxCollider2D blockCollider;
- private bool isClose = true;
- private Animator _animator;
- private BoxCollider2D _collider;
- private static readonly int Close = Animator.StringToHash("close");
- private static readonly int Open = Animator.StringToHash("open");
-   public AudioSource sound;
+    private bool isClose = true;
+    private Animator _animator;
+    private BoxCollider2D _collider;
+    private static readonly int Close = Animator.StringToHash("close");
+    private static readonly int Open = Animator.StringToHash("open");
+    public AudioSource sound;
 
     void Start()
     {
-      _animator = GetComponent<Animator>();
-      _collider = GetComponent<BoxCollider2D>();
+        _animator = GetComponent<Animator>();
+        _collider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-
     public void InteractWithDoor()
-   {
-       isClose = !isClose;
-       if(isClose)
+    {
+        isClose = !isClose;
+        if (isClose)
             _animator.SetTrigger(Close);
-       else 
-           _animator.SetTrigger(Open);
+        else
+            _animator.SetTrigger(Open);
         sound.Play();
-       blockCollider.enabled = isClose;
-   }
-   
+        blockCollider.enabled = isClose;
+    }
 }
